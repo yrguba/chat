9191e930592e46ea16776397cfb8ee535f820150
Юрий Губа <yuriy@MacBook-Pro-Urij.local>
2022-08-31 15:30:15 +0300

    fb

--- src/main.ts
+++ src/main.ts
@@ -14,19 +14,16 @@ import { FilesModule } from "./files/files.module";
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from "firebase-admin";
 
+const serviceAccount = require("../fb.json");
+console.log(serviceAccount);
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService: ConfigService = app.get(ConfigService);
 
-  const adminConfig: ServiceAccount = {
-    "projectId": configService.get<string>('FIREBASE_PROJECT_ID'),
-    "privateKey": configService.get<string>('FIREBASE_PRIVATE_KEY')
-        .replace(/\\n/g, '\n'),
-    "clientEmail": configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-  };
   // Initialize the firebase admin app
   admin.initializeApp({
-    credential: admin.credential.cert(adminConfig),
+    credential: admin.credential.cert(serviceAccount)
     //databaseURL: "https://xxxxx.firebaseio.com",
   });
 
