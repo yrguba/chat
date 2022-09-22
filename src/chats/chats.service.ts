d313bfe44792a29d00eb16766191c25c8b5e21f5
Юрий Губа <yuriy@MacBook-Pro-Urij.local>
2022-09-22 16:06:45 +0300

    fix chats bugs

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -348,7 +348,7 @@ export class ChatsService {
                 .where('chats.users @> :users', {users: [user_id]})
                 //.andWhere("LOWER(chats.name) like LOWER(:name)", { name:`%${options.like.toLowerCase()}%` })
                 .leftJoinAndSelect('chats.message', 'message', null,{'order': 'desc'})
-                .orderBy('chats.updated_at', 'ASC')
+                .orderBy('chats.updated_at', 'DESC')
                 //.addOrderBy('message.created_at', 'DESC')
                 .getMany();
 
