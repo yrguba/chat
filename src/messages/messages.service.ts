00c21e1939d2e8e93b38c588d1abdb37ebe22be7
Raybul <93190553+Raybul@users.noreply.github.com>
2023-01-18 14:03:23 +0300

    upd

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -447,7 +447,7 @@ export class MessagesService {
                     ),
                     priority: "max",
                     sound: "default",
-                    thread_id: String(chat_id),
+                    "thread-id": String(chat_id),
                   },
                   data: {
                     text: await this.getMessageContent(user_id, message),
